a92583f51e519085bedb3d23988eda6904d936f5
Thomas Stephenson <ovangle@gmail.com>
2014-06-10 17:30:45 +1000

    test: Added connection tests to `server_tests` runner

---
+++ test/connection/all_tests.dart
@@ -0,0 +1,9 @@
+library connection_tests;
+
+import 'package:unittest/unittest.dart';
+import 'object_transfer_test.dart' as object_transfer;
+
+void main() {
+  object_transfer.main();
+
+}
--- test/server_tests.dart
+++ test/server_tests.dart
@@ -1,11 +1,13 @@
 library server_tests;
 
+import 'connection/all_tests.dart' as connection;
 import 'json/all_tests.dart' as json;
 import 'source/server_tests.dart' as source;
 import 'utils/all_tests.dart' as utils;
 
 
 void main() {
+  connection.main();
   json.main();
   source.main();
   utils.main();
