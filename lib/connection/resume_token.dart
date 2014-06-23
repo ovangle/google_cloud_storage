cbc8d968d9848ad6c2b9e6b3d846b0db677035e9
Thomas Stephenson <ovangle@gmail.com>
2014-06-24 00:35:05 +1000

    feat: Serialization of resume tokens

--- lib/connection/resume_token.dart
+++ lib/connection/resume_token.dart
@@ -25,9 +25,17 @@ class ResumeToken {
    */
   final int type;
 
+  /**
+   * The range (inclusive of start and end bytes) that has already been uploaded
+   * to the server when this token was created.
+   *
+   * A `null` range indicates that no bytes have yet been uploaded to the server.
+   */
   final Range range;
 
-  final rpcResponse;
+  final RpcResponse rpcResponse;
+
+  final String selector;
 
   bool get isInit => type == TOKEN_INIT;
   bool get isComplete => type == TOKEN_COMPLETE;
@@ -37,8 +45,53 @@ class ResumeToken {
    */
   final Uri uploadUri;
 
-  ResumeToken(this.type, this.uploadUri, {this.range}) : rpcResponse = null;
+  ResumeToken(this.type, this.uploadUri, this.selector, {this.range}) : rpcResponse = null;
 
   ResumeToken.fromToken(ResumeToken token, this.type, {this.range, this.rpcResponse}):
-    this.uploadUri = token.uploadUri;
+    this.uploadUri = token.uploadUri,
+    this.selector = token.selector;
+
+  /**
+   * Deserialize a [ResumeToken].
+   */
+  factory ResumeToken.fromJson(Map<String,dynamic> json) {
+    var type = json['type'];
+    if (type == null) {
+      throw new TokenSerializationException("No 'type'");
+    }
+    if (type < 0 || type > 2) {
+      throw new TokenSerializationException("Invalid value for type field");
+    }
+    if (json['uploadUri'] == null)
+      throw new TokenSerializationException("No 'uploadUri'");
+    return new ResumeToken(
+        type,
+        Uri.parse(json['uploadUri']),
+        json['selector'] != null ? json['selector'] : '*',
+        range: (json['range'] != null) ? Range.parse(json['range']) : null
+    );
+  }
+
+  /**
+   * Serialize a [ResumeToken] as a JSON map.
+   */
+  Map<String,dynamic> toJson() {
+    var json = {
+        'type': type,
+        'uploadUri': uploadUri.toString(),
+        'selector': selector
+    };
+    if (range != null)
+      json['range'] = range.toString();
+    return json;
+  }
+}
+
+class TokenSerializationException implements Exception {
+  final String message;
+
+  TokenSerializationException(this.message);
+
+  toString() => message;
+
 }
